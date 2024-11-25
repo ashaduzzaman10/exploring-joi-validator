dac9513860cc1ecbe15f610e98fecbe01bc0925f
ashaduzzaman10 <devakashtbo10@gmail.com>
2024-11-26 02:26:26 +0600

    feat : usr creation route complite

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
+const userModel = require("./models/userModel");
+const bcrypt = require("bcrypt");
 
 // application middleware
 
@@ -11,6 +13,54 @@ app.use([
 	express.json(),
 	express.urlencoded({ extended: true }),
 ]);
+
+// user validation
+app.post("/api/register", async (req, res) => {
+	try {
+		const { name, email, password } = req.body;
+		const User = await userModel.findOne({ name: name, email: email });
+		if (User)
+			return res.status(400).json({
+				success: false,
+				data: "user already exist",
+				user: User,
+			});
+
+		if (!User) {
+			const salt = 10;
+			bcrypt.hash(password, salt, async (err, hash) => {
+				if (err) {
+					res.status(500).json({
+						success: false,
+						data: {
+							message: "something wrong when user was created",
+							error: err.message,
+						},
+					});
+				}
+				if (!err) {
+					const createUser = new userModel({
+						name,
+						email,
+						password: hash,
+					});
+					await createUser.save();
+					res.status(201).json({
+						success: true,
+						data: { message: "user created successfully" },
+					});
+				}
+			});
+		}
+	} catch (error) {
+		res.status(500).json({
+			success: true,
+			data: {
+				error: "error.message",
+			},
+		});
+	}
+});
 
 // home route
 app.get("/", (req, res) => {
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-require( "dotenv" ).config();
-const app = require( "./app" );
+require("dotenv").config();
+const app = require("./app");
+const dbConnection = require("./db/dbConnection");
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-	console.log(
-		`server is running on ${PORT} and your address is : http://localhost:${PORT}`
-	);
+	dbConnection(),
+		console.log(
+			`server is running on ${PORT} and your address is : http://localhost:${PORT}`
+		);
 });
